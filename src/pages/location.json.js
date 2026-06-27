751e7cee9594598bd61f05b5f4ae96eb16afd6d3
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-28 02:09:24 +0330

    Derive location map profiles from source contract

--- src/pages/location.json.js
+++ src/pages/location.json.js
@@ -1,16 +1,19 @@
+import { authoritySignals } from '../data/authoritySignals.mjs';
 import { location } from '../data/location.mjs';
 import { site } from '../data/site.mjs';
+import { getUrlsByUse } from '../lib/sourceClassifier.mjs';
 
 export function GET() {
   const body = {
-    schema: 'ghezelbaash.location.astro.v1',
+    schema: 'ghezelbaash.location.astro.v2.source_contract',
     canonicalWebsite: site.canonicalBase + '/',
     address: location.canonicalAddressFa,
     telephone: location.telephone,
     googleMaps: location.googleMapsCid,
     googleMapsPlace: location.googleMapsPlace,
     openStreetMap: location.openStreetMap,
-    openStreetMapNodeId: location.openStreetMapNodeId
+    openStreetMapNodeId: location.openStreetMapNodeId,
+    mapProfiles: getUrlsByUse(authoritySignals, 'clinic', 'hasMap')
   };
 
   return new Response(JSON.stringify(body, null, 2) + '\n', {
