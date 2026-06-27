98b44675d5bb28d993e3052b4a25d88ac054a0dd
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-27 07:38:40 +0330

    Add external profile registry

---
+++ src/data/externalProfiles.mjs
@@ -0,0 +1,32 @@
+export const externalProfiles = {
+  verifiedSameAs: [
+    'https://www.wikidata.org/wiki/Q140287622',
+    'https://www.wikidata.org/wiki/Q140288589',
+    'https://www.wikidata.org/wiki/Q140304972',
+    'https://orcid.org/0009-0001-9346-8475',
+    'https://www.ncbi.nlm.nih.gov/myncbi/saeed.ghezelbash.1/bibliography/public/',
+    'https://membersearch.irimc.org/member/profile?id=9efaaf28-52ff-49ad-8d45-be6e48c4fa3e',
+    'https://www.linkedin.com/in/saeed-ghezelbash-93310a96/',
+    'https://github.com/medicaldoctor91/doctor-ghezelbaash',
+    'https://www.instagram.com/doctor.ghezelbaash/',
+    'https://www.google.com/maps?cid=12350483144643112463',
+    'https://www.openstreetmap.org/node/13530287096',
+    'https://about.me/ghezelbaash',
+    'https://linktr.ee/Doctor.ghezelbaash',
+    'https://huggingface.co/datasets/doctor-ghezelbaash/dr-saeid-ghezelbaash-entity-data',
+    'https://doi.org/10.5281/zenodo.18765169'
+  ],
+  observedButNeedsDirectVerification: [
+    { platform: 'Telegram', url: 'https://t.me/ghezelbaash' },
+    { platform: 'X', url: 'https://twitter.com/Qezelbaash' },
+    { platform: 'Facebook', source: 'Linktree social list' },
+    { platform: 'TikTok', source: 'Linktree social list' },
+    { platform: 'Threads', source: 'Linktree social list' },
+    { platform: 'Pinterest', source: 'Linktree social list' },
+    { platform: 'YouTube', source: 'Linktree social list' }
+  ],
+  policy: {
+    sameAs: 'Use only directly verified profile URLs that represent the same person, clinic or public dataset entity.',
+    authoritySignals: 'Use broader mentions, media pages, directories and coverage sources as subjectOf or authoritySignal, not as sameAs.'
+  }
+};
