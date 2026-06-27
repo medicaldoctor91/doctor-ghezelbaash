d9ace95bad0904aa10fc9ad8f1f410225585d548
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-28 03:18:59 +0330

    Integrate reputation in location endpoint

--- src/pages/location.json.js
+++ src/pages/location.json.js
@@ -1,18 +1,27 @@
 import { authoritySignals } from '../data/authoritySignals.mjs';
 import { location } from '../data/location.mjs';
+import { googleMapsReputation } from '../data/reputation.mjs';
 import { site } from '../data/site.mjs';
 import { getUrlsByUse } from '../lib/sourceClassifier.mjs';
 
 export function GET() {
   const body = {
-    schema: 'ghezelbaash.location.astro.v2.source_contract',
+    schema: 'ghezelbaash.location.astro.v3.reputation_integrated',
     canonicalWebsite: site.canonicalBase + '/',
     address: location.canonicalAddressFa,
+    streetAddress: location.streetAddressFa,
+    addressLocality: location.addressLocality,
+    addressRegion: location.addressRegion,
+    postalCode: location.postalCode,
+    addressCountry: location.addressCountry,
     telephone: location.telephone,
+    priceRange: location.priceRange,
     googleMaps: location.googleMapsCid,
     googleMapsPlace: location.googleMapsPlace,
     openStreetMap: location.openStreetMap,
     openStreetMapNodeId: location.openStreetMapNodeId,
+    geo: location.geo,
+    googleMapsReputation,
     mapProfiles: getUrlsByUse(authoritySignals, 'clinic', 'hasMap')
   };
 
