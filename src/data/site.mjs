3a9d082fa8346d5a4350be4be081a23ddcf8067d
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-27 06:51:31 +0330

    Add site metadata for Astro SEO outputs

--- src/data/site.mjs
+++ src/data/site.mjs
@@ -8,6 +8,11 @@ export const site = {
   phoneDisplay: '09308209494',
   phoneE164: '+989308209494',
   mapsCid: 'https://www.google.com/maps?cid=12350483144643112463',
+  logo: '/logo.png',
+  image: '/doctor.jpg',
+  locale: 'fa-IR',
+  city: 'Kermanshah',
+  country: 'IR',
   pages: {
     home: '/',
     person: '/dr-saeed-ghezelbash/',
@@ -16,6 +21,15 @@ export const site = {
     contact: '/contact/',
     evidence: '/evidence/',
     kg: '/kg/'
+  },
+  pageLabels: {
+    '/': 'خانه',
+    '/dr-saeed-ghezelbash/': 'دکتر سعید قزلباش',
+    '/dr-saeed-ghezelbash-aesthetic-clinic/': 'کلینیک زیبایی دکتر سعید قزلباش',
+    '/services/': 'خدمات',
+    '/contact/': 'تماس',
+    '/evidence/': 'شواهد',
+    '/kg/': 'گراف دانش'
   },
   sameAs: {
     person: [
@@ -32,8 +46,16 @@ export const site = {
       'https://www.instagram.com/doctor.ghezelbaash/',
       'https://www.google.com/maps?cid=12350483144643112463',
       'https://www.openstreetmap.org/node/13530287096'
+    ],
+    kg: [
+      'https://www.wikidata.org/wiki/Q140304972',
+      'https://huggingface.co/datasets/doctor-ghezelbaash/dr-saeid-ghezelbaash-entity-data',
+      'https://doi.org/10.5281/zenodo.18765169',
+      'https://github.com/medicaldoctor91/doctor-ghezelbaash'
     ]
   }
 };
 
 export const absoluteUrl = (path) => new URL(path, site.canonicalBase).toString();
+export const canonicalImage = (path = site.image) => absoluteUrl(path);
+export const routeLabel = (path) => site.pageLabels[path] || path.replaceAll('/', '').replaceAll('-', ' ') || site.nameFa;
