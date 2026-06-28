98da937c7d9be862da412d78602c9c44c4a98b7b
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-29 02:36:44 +0330

    Clean primary graph nodes before response

--- src/pages/graph-ghezelbaash-final.jsonld.js
+++ src/pages/graph-ghezelbaash-final.jsonld.js
@@ -1,7 +1,9 @@
 import { buildGlobalGraph } from '../lib/globalGraph.mjs';
+import { applySchemaOrgCompliancePass as cleanGraphNodes } from '../lib/schemaOrgCompliancePass.mjs';
 
 export function GET() {
   const graph = buildGlobalGraph();
+  cleanGraphNodes(graph['@graph'] || []);
 
   return new Response(JSON.stringify(graph, null, 2) + '\n', {
     headers: { 'Content-Type': 'application/ld+json; charset=utf-8' }
