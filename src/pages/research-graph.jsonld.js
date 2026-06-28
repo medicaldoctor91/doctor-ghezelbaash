29669ab7341f3806713bb61fb18b6f4ddba07824
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-28 20:09:02 +0330

    Retire research graph JSON-LD endpoint

--- src/pages/research-graph.jsonld.js
+++
@@ -1,7 +0,0 @@
-import { buildResearchGraph } from '../lib/researchGraph.mjs';
-
-export function GET() {
-  return new Response(JSON.stringify(buildResearchGraph(), null, 2) + '\n', {
-    headers: { 'Content-Type': 'application/ld+json; charset=utf-8' }
-  });
-}
