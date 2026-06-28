8f5d6f623ec9b54f11f977b4eb391027cc1a7b4d
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-29 02:57:31 +0330

    Enrich primary graph with local actions

--- src/pages/graph-ghezelbaash-final.jsonld.js
+++ src/pages/graph-ghezelbaash-final.jsonld.js
@@ -1,9 +1,11 @@
 import { buildGlobalGraph } from '../lib/globalGraph.mjs';
+import { applyLocalBusinessActionPass as enrichLocalGraph } from '../lib/localBusinessActionPass.mjs';
 import { applySchemaOrgCompliancePass as cleanGraphNodes } from '../lib/schemaOrgCompliancePass.mjs';
 
 export function GET() {
   const graph = buildGlobalGraph();
   cleanGraphNodes(graph['@graph'] || []);
+  enrichLocalGraph(graph['@graph'] || []);
 
   return new Response(JSON.stringify(graph, null, 2) + '\n', {
     headers: { 'Content-Type': 'application/ld+json; charset=utf-8' }
