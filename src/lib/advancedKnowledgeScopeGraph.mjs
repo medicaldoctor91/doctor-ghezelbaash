c4a71a82f8b8372083569323f46717ea6fa82ef7
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-30 17:12:40 +0330

    Add empty compatibility graph module

---
+++ src/lib/advancedKnowledgeScopeGraph.mjs
@@ -0,0 +1,11 @@
+export function advancedKnowledgeTermReferences() {
+  return [];
+}
+
+export function buildAdvancedKnowledgeTermSet() {
+  return null;
+}
+
+export function buildAdvancedKnowledgeTerms() {
+  return [];
+}
