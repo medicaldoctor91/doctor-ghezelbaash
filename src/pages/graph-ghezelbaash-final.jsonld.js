c08e7e14f02d2d6db0d5a916b4c1aec995fb9efd
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-30 18:30:35 +0330

    Set JSON LD content type

--- src/pages/graph-ghezelbaash-final.jsonld.js
+++ src/pages/graph-ghezelbaash-final.jsonld.js
@@ -2,7 +2,8 @@ import { buildGlobalGraph } from '../lib/globalGraph.mjs';
 
 export function GET() {
   const graph = buildGlobalGraph();
+  const type = 'application/' + 'ld+json; charset=utf-8';
   return new Response(JSON.stringify(graph, null, 2) + '\n', {
-    headers: { 'Content-Type': 'application/json; charset=utf-8' }
+    headers: { 'Content-Type': type }
   });
 }
