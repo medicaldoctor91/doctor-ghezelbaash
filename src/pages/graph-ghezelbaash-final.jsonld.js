de071c4200a2983e5d013168b28944505cb60cb7
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-06-30 18:33:09 +0330

    Restore route graph passes

--- src/pages/graph-ghezelbaash-final.jsonld.js
+++ src/pages/graph-ghezelbaash-final.jsonld.js
@@ -1,7 +1,11 @@
 import { buildGlobalGraph } from '../lib/globalGraph.mjs';
+import { applyLocalBusinessActionPass } from '../lib/localBusinessActionPass.mjs';
+import { applySchemaOrgCompliancePass } from '../lib/schemaOrgCompliancePass.mjs';
 
 export function GET() {
   const graph = buildGlobalGraph();
+  applySchemaOrgCompliancePass(graph['@graph'] || []);
+  applyLocalBusinessActionPass(graph['@graph'] || []);
   const type = 'application/' + 'ld+json; charset=utf-8';
   return new Response(JSON.stringify(graph, null, 2) + '\n', {
     headers: { 'Content-Type': type }
