7c9662488ddd618d287e1b58963594c6c46552fe
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-07-01 07:44:02 +0330

    Align site routes with AstroWind main structure

--- src/data/site.mjs
+++ src/data/site.mjs
@@ -1,14 +1,13 @@
 export const site = {
   canonicalBase: 'https://www.ghezelbaash.ir',
   nameFa: 'کلینیک زیبایی دکتر سعید قزلباش',
-  nameEn: 'Dr. Saeed Ghezelbash Aesthetic Clinic',
+  nameEn: 'Dr. Saeed Ghezelbaash Aesthetic Clinic',
   personFa: 'دکتر سعید قزلباش',
-  personEn: 'Dr. Saeed Ghezelbash',
+  personEn: 'Dr. Saeed Ghezelbaash',
   instagram: 'https://www.instagram.com/doctor.ghezelbaash/',
   phoneDisplay: '09308209494',
   phoneE164: '+989308209494',
   mapsCid: 'https://www.google.com/maps?cid=12350483144643112463',
-  mapsSearch: 'https://www.google.com/maps/search/?api=1&query=کلینیک%20زیبایی%20دکتر%20قزلباش%20کرمانشاه&query_place_id=ChIJBTOYDOTt-j8RD-7mAPy6Zas',
   logo: '/logo.png',
   image: '/doctor.jpg',
   locale: 'fa-IR',
@@ -16,29 +15,28 @@ export const site = {
   country: 'IR',
   pages: {
     home: '/',
-    person: '/dr-saeed-ghezelbash/',
-    clinic: '/dr-saeed-ghezelbash-aesthetic-clinic/',
+    person: '/about/',
+    clinic: '/about/',
     services: '/services/',
     contact: '/contact/',
-    evidence: '/evidence/',
-    kg: '/kg/'
+    evidence: '/services/',
+    kg: '/'
   },
   pageLabels: {
     '/': 'خانه',
-    '/dr-saeed-ghezelbash/': 'دکتر سعید قزلباش',
-    '/dr-saeed-ghezelbash-aesthetic-clinic/': 'کلینیک زیبایی دکتر سعید قزلباش',
+    '/about/': 'درباره دکتر و کلینیک',
     '/services/': 'خدمات',
     '/contact/': 'تماس',
-    '/evidence/': 'شواهد',
-    '/kg/': 'گراف دانش'
+    '/botox/': 'بوتاکس',
+    '/filler/': 'فیلر',
+    '/thread-lift/': 'لیفت نخ',
+    '/skin-rejuvenation/': 'جوانسازی پوست'
   },
   sameAs: {
     person: [
       'https://www.wikidata.org/wiki/Q140287622',
       'https://orcid.org/0009-0001-9346-8475',
       'https://www.ncbi.nlm.nih.gov/myncbi/saeed.ghezelbash.1/bibliography/public/',
-      'https://membersearch.irimc.org/member/profile?id=9efaaf28-52ff-49ad-8d45-be6e48c4fa3e',
-      'https://www.linkedin.com/in/saeed-ghezelbash-93310a96/',
       'https://github.com/medicaldoctor91/doctor-ghezelbaash',
       'https://www.instagram.com/doctor.ghezelbaash/'
     ],
@@ -46,12 +44,10 @@ export const site = {
       'https://www.wikidata.org/wiki/Q140288589',
       'https://www.instagram.com/doctor.ghezelbaash/',
       'https://www.google.com/maps?cid=12350483144643112463',
-      'https://www.google.com/maps/search/?api=1&query=کلینیک%20زیبایی%20دکتر%20قزلباش%20کرمانشاه&query_place_id=ChIJBTOYDOTt-j8RD-7mAPy6Zas',
       'https://www.openstreetmap.org/node/13530287096'
     ],
     kg: [
       'https://www.wikidata.org/wiki/Q140304972',
-      'https://huggingface.co/datasets/doctor-ghezelbaash/dr-saeid-ghezelbaash-entity-data',
       'https://doi.org/10.5281/zenodo.18765169',
       'https://github.com/medicaldoctor91/doctor-ghezelbaash'
     ]
