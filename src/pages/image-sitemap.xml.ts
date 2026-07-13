e61225fa81cd80a852f5cc053ebb5fe6e2cc6f69
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-07-13 20:53:24 +0330

    refactor(image): consolidate visual assets under homepage

--- src/pages/image-sitemap.xml.ts
+++ src/pages/image-sitemap.xml.ts
@@ -1,6 +1,5 @@
 import { site } from '~/domain/entities';
 import { galleryImages, videos } from '~/domain/media.mjs';
-import { videoWatchUrl } from '~/domain/url-architecture.mjs';
 
 export const prerender = true;
 
@@ -18,12 +17,10 @@ export function GET() {
     ...videos.map((video: { thumbnail: string }) => `${site.url}${video.thumbnail.slice(1)}`),
   ];
 
-  const homepageEntry = `  <url>\n    <loc>${site.url}</loc>\n${homepageImages
+  const images = homepageImages
     .map((loc) => `    <image:image>\n      <image:loc>${esc(loc)}</image:loc>\n    </image:image>`)
-    .join('\n')}\n  </url>`;
+    .join('\n');
 
-  const watchEntries = videos.map((video: any) => `  <url>\n    <loc>${esc(videoWatchUrl(site.url, video.id))}</loc>\n    <image:image>\n      <image:loc>${esc(`${site.url}${video.thumbnail.slice(1)}`)}</image:loc>\n      <image:title>${esc(video.title)}</image:title>\n    </image:image>\n  </url>`).join('\n');
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n${homepageEntry}\n${watchEntries}\n</urlset>\n`;
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n  <url>\n    <loc>${site.url}</loc>\n${images}\n  </url>\n</urlset>\n`;
   return new Response(xml, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
 }
