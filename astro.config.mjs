eda06da950abc10f15284270c46aa14ac193a019
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-07-29 06:33:16 +0330

    Externalize production CSS for earlier paint

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [normalizeBuiltHtml()],
   build: {
     format: 'directory',
-    inlineStylesheets: 'always',
+    inlineStylesheets: 'never',
   },
   vite: {
     build: {
