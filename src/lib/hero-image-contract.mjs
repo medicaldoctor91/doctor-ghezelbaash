a93ddadc37c1b0b58ea2507b4368e67c1f81df14
Dr. Saeed Ghezelbaash | دکتر سعید قزلباش <medicaldoctor91@gmail.com>
2026-08-23 07:46:33 +0330

    refactor: centralize canonical hero preload assets

--- src/lib/hero-image-contract.mjs
+++ src/lib/hero-image-contract.mjs
@@ -1,6 +1,8 @@
 export const HERO_IMAGE_SIZES='(max-width: 720px) and (max-width: 79rem) calc(100vw - 2.56rem), (max-width: 720px) 76.44rem, (max-width: calc(45.19828rem + 2.1978px)) 18rem, (max-width: 80rem) calc(41.86vw - .92rem - .92px), (max-width: 100rem) calc(35.88rem - 4.14vw - .92px), calc(31.74rem - .92px)';
 export const HERO_FIGURE_TOTAL_BORDER_PX=2;
 export const HERO_IMAGE_SIZES_TOKEN='{{HERO_IMAGE_SIZES}}';
+export const HERO_PRELOAD_HREF='/media/images/physician/saeed-ghezelbash-portrait-delivery-640.7b2b6ac2affa.avif';
+export const HERO_PRELOAD_SRCSET='/media/images/physician/saeed-ghezelbash-portrait-delivery-640.7b2b6ac2affa.avif 640w, /media/images/physician/saeed-ghezelbash-portrait-960.497fc78613ac.avif 960w, /media/images/physician/saeed-ghezelbash-portrait-1600.665436f5bf39.avif 1600w';
 
 const tokenCount=value=>String(value).split(HERO_IMAGE_SIZES_TOKEN).length-1;
 
